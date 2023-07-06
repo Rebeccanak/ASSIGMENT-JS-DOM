8e8164550ca22a1282b7a78a2e49cfd7b4162de9
Rebecca Nakachwa <rebeccanakachwaaa@gmail.com>
2023-07-06 07:44:04 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from "./Login";
+import Products from './Products/product';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div>
+    
+    <Login/>
+    <Products/>
+
+<Navbar/>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/BlogPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from '../Navbar'
+
+function BlogPage (){
+    return(
+        <div>
+        <Navbar></Navbar>
+        <div>Product Page</div>
+        </div>
+    )
+
+}
+export default BlogPage
